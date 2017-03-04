3f8479dfa8c54a2239eac2e82d7750367b7a016c
devstopfix <devstopfix@gmail.com>
2017-03-04 12:06:43 +0000

    chore: empty project

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject evrythng.spec "0.17.63"
+  :description "Unofficial specification of the EVRYTHNG.com REST API"
+  :url "https://github.com/devstopfix/evrythng.spec"
+  :license {:name "Apache License v2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
+  :profiles {:test
+             {:dependencies [[org.clojure/test.check "LATEST"]]}})
---
+++ src/evrythng/spec.clj
@@ -0,0 +1,6 @@
+(ns evrythng.spec)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
